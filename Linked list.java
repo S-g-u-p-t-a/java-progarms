3f9fa1743c56611c1d1660e4cd6164c9208fea29
Shailesh9926 <91823945+Shailesh9926@users.noreply.github.com>
2021-10-15 22:42:06 +0530

    Create Linked list.java

---
+++ Linked list.java
@@ -0,0 +1,13 @@
+// Import the LinkedList class
+import java.util.LinkedList;
+
+public class Main {
+  public static void main(String[] args) {
+    LinkedList<String> cars = new LinkedList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+  }
+}
